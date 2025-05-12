473209e1525a79c76a9bca786d3c002dc291b928
文峰SEC <41097521+wffwf@users.noreply.github.com>
2025-05-12 10:44:14 +0800

    增加一个特殊情况的硬编码

--- si_4a.py
+++ si_4a.py
@@ -19,6 +19,17 @@ def fenxi_4A(step,total,current_time):
     """处理4a全量资产数据生成4A资产.xlsx"""
     filename = FILE_PATHS['input']['4A平台']
     iris = pd.read_excel(filename)
+    # 硬编码剔除4A中4个IP报备不合规的资产
+    # 希望能够在2026年1月份各项通报工作尚未正式开展时完成修改
+    # 定义需要过滤的IP地址列表
+    filtered_ips = ['221.178.219.219', '221.178.219.218', '221.178.219.175', '221.178.219.176']
+    # 将字符串转换为 pandas 时间戳
+    current_timestamp = pd.to_datetime(current_time, format="%Y%m%d%H%M%S")
+    # 比较时间
+    if current_timestamp < pd.Timestamp('2026-01-01'):
+        # 从iris中过滤掉'资源IP'列中包含特定IP的行
+        iris = iris[~iris['资源IP'].isin(filtered_ips)]    
+    # 硬编码结束
     result = fenxi_4A_byIP(iris)
     host_data = fenxi_4A_byIPandHost(iris)
 
@@ -31,11 +42,12 @@ def read_from_4afile():
     result,host_data = (fenxi_4A(2,3,current_time))
     # print(host_data)
     return result
-    # 保存结果
-    original_result_path = FILE_PATHS['output']['4A自用']
-    timestamp_result_path = original_result_path.replace(".xlsx", f"_{current_time}.xlsx")  
-    result.to_excel(timestamp_result_path, index=False)    
 
 # ------------------------- 测试main函数 -------------------------
 if __name__ == "__main__":
-    read_from_4afile()
+    result = read_from_4afile()
+    # 保存结果
+    current_time = time.strftime("%Y%m%d%H%M%S", time.localtime())  # 获取当前时间戳     
+    original_result_path = FILE_PATHS['output']['4A自用']
+    timestamp_result_path = original_result_path.replace(".xlsx", f"_{current_time}.xlsx")  
+    result.to_excel(timestamp_result_path, index=False)     
